04ca0402ac3a9ef0786b21faa765bc25915834c2
Haiminh2023 <122427348+Haiminh2023@users.noreply.github.com>
2026-02-12 10:39:02 +0700

    Update include.js

--- js/include.js
+++ js/include.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 <polyline points="7 10 12 15 17 10"/>
                 <line x1="12" y1="15" x2="12" y2="3"/>
             </svg>
-            Tải plugin
+            <span class="download-text">Tải plugin mới nhất</span>
         `;
         document.body.appendChild(button);
     }
